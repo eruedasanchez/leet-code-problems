3fd47cf17a0f8e070b9079ba7da4b7da59f0c2c8
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-02-27 22:04:01 -0300

    Resolve removeElement problem

--- src/001-remove-element/removeElement.ts
+++ src/001-remove-element/removeElement.ts
@@ -1,1 +1,13 @@
-console.log('resolve exercise');
+function removeElement(nums: (number | '_')[], val: number): number {
+    let distinctVal: '_'[] = [];  
+    
+    for (let index = 0; index < nums.length; index++) {
+        while(nums[index] === val){
+            nums.splice(index, 1);
+            distinctVal.push('_');
+        }
+    }
+    
+    nums = [...nums, ...distinctVal];
+    return nums.length - distinctVal.length;
+};
