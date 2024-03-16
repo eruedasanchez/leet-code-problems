5606063004f24f4856df7e37e86658ebeffae623
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-03-16 18:48:55 -0300

    Resolve Max Profit problem

--- src/006-max-profit/maxProfit.ts
+++ src/006-max-profit/maxProfit.ts
@@ -1,15 +1,23 @@
-const maxProfit = (prices: number[]) : number => {
-    let maximProfit = 0;
-    for (let purchaseDay = 0; purchaseDay < prices.length; purchaseDay++) {
-        for (let saleDay = purchaseDay + 1; saleDay < prices.length; saleDay++) {
-            if(prices[saleDay] - prices[purchaseDay] > 0 &&
-            prices[saleDay] - prices[purchaseDay] > maximProfit){
-                maximProfit = prices[saleDay] - prices[purchaseDay];
-            }
-        }
-        
-    }
-    return maximProfit;
-}
+const MAX_VALUE_PRICE = 10000;
 
-// ver opcion d&q
+const maxProfit = (prices: number[]) : number => {
+    if(prices.length === 1) return 0;
+
+    let half = Math.trunc(prices.length / 2);
+    let leftValues: number[] = prices.slice(0, half);
+    let rightValues: number[] = prices.slice(half, prices.length);
+    let maxLeftProfit = maxProfit(leftValues);
+    let maxRightProfit = maxProfit(rightValues);
+
+    let maxLeftRightProfit = 0, minLeft = MAX_VALUE_PRICE;
+    
+    for (let l = 0; l < leftValues.length; l++) {
+        minLeft = Math.min(minLeft, leftValues[l]);
+    }
+    
+    for (let r = 0; r < rightValues.length; r++) {
+        maxLeftRightProfit = Math.max(maxLeftRightProfit, rightValues[r] - minLeft);
+    }
+    
+    return Math.max(maxLeftProfit, maxLeftRightProfit, maxRightProfit);
+}
