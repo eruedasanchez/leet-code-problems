13d11461f4e9f416eff9edd0f99f85676bd2fa12
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-03-16 15:26:28 -0300

    Resolve Remove Duplicates II Problem

--- src/003-remove-duplicates-II/removeDuplicatesII.ts
+++ src/003-remove-duplicates-II/removeDuplicatesII.ts
@@ -1,18 +1,35 @@
+const MAX_OCURRENCES = 2;
+const UNDERSCORE = '_';
+
 const removingDuplicates = (nums: (number | '_')[]) : number => {
-    let i = 0, j = 1, distinctCounter = 1;
-    while(j < nums.length) {
-        if(nums[i] === nums[j]){
-            nums[j] = '_';
+    let i = 0, j = 0;
+    let currentNumber = nums[i], ocurrencesCurrentNumber = 0;
+    let allowedNumbers = 0;
+    
+    while(j < nums.length){
+        if(currentNumber === nums[j] && ocurrencesCurrentNumber < MAX_OCURRENCES){
             j++;
+            ocurrencesCurrentNumber++;
+            allowedNumbers++;
+        } else if(currentNumber !== nums[j]){
+            i = j;
+            currentNumber = nums[i];
+            ocurrencesCurrentNumber = 0;
         } else {
-            i++;
-            distinctCounter++;
-            j === i ? j++ : nums[i] = nums[j];
+            let k = j;
+            while(nums[k] === nums[i]) {
+                nums.splice(k, 1);
+            }
+            i = k;
+            j = i;
+            currentNumber = nums[i];
+            ocurrencesCurrentNumber = 0;
         }
     }
-    return distinctCounter;
+    
+    for (let m = 0; m < allowedNumbers; m++) {
+        nums.push('_');
+    }
+    
+    return allowedNumbers;
 }
-
-
-// Entrada: números = [1,1,1,2,2,3]
-// Salida: 5, números = [1,1,2,2,3,_]
