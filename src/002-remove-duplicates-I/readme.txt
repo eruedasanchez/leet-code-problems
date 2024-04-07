6260c7c125498b5085dbc16e3fd0f8e8341654be
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-04-06 22:31:05 -0300

    Update README.md

--- src/002-remove-duplicates-I/readme.txt
+++ src/002-remove-duplicates-I/readme.txt
@@ -1,3 +1,5 @@
+ENG 
+
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
@@ -35,6 +37,51 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 
 Constraints:
 
-1 <= nums.length <= 3 * 104
+1 <= nums.length <= 3 * 10^4
 -100 <= nums[i] <= 100
 nums is sorted in non-decreasing order.
+
+* ------------------------------------------------------------ *
+
+ESP
+
+Dada una matriz de números enteros ordenados en orden no decreciente, elimine los duplicados en el lugar de modo que cada elemento único aparezca solo una vez. El orden relativo de los elementos debe mantenerse igual. Luego devuelve el número de elementos únicos en números.
+
+Considere que la cantidad de elementos únicos de nums es k; para ser aceptado, debe hacer lo siguiente:
+
+Cambie los números de la matriz de modo que los primeros k elementos de nums contengan los elementos únicos en el orden en que estaban presentes en nums inicialmente. Los elementos restantes de nums no son tan importantes como el tamaño de nums.
+Regresar k.
+Juez personalizado:
+
+El juez probará su solución con el siguiente código:
+
+int[] números = [...]; // matriz de entrada
+int[] números esperados = [...]; // La respuesta esperada con la longitud correcta
+
+int k = eliminarDuplicados(numeros); // Llama a tu implementación
+
+afirmar k == números esperados.longitud;
+para (int i = 0; i < k; i++) {
+    afirmar números [i] == números esperados [i];
+}
+Si se aprueban todas las afirmaciones, se aceptará su solución.
+
+Ejemplo 1:
+
+Entrada: números = [1,1,2]
+Salida: 2, números = [1,2,_]
+Explicación: Su función debería devolver k = 2, siendo los dos primeros elementos de nums 1 y 2 respectivamente.
+No importa lo que dejes más allá de la k devuelta (de ahí que sean guiones bajos).
+Ejemplo 2:
+
+Entrada: números = [0,0,1,1,1,2,2,3,3,4]
+Salida: 5, números = [0,1,2,3,4,_,_,_,_,_]
+Explicación: Su función debería devolver k = 5, siendo los primeros cinco elementos de nums 0, 1, 2, 3 y 4 respectivamente.
+No importa lo que dejes más allá de la k devuelta (de ahí que sean guiones bajos).
+ 
+
+Restricciones:
+
+1 <= números.longitud <= 3 * 10^4
+-100 <= números[i] <= 100
+nums se ordena en orden no decreciente.
--- src/013-gas-station/gasStation.ts
+++ src/013-gas-station/gasStation.ts
@@ -1,49 +1,77 @@
 const canCompleteCircuit = (gas: number[], cost: number[]) : number => {
-    let start = -1, end = -1;
+    let start = -1, end = -1, prevEnd = -1;
     let tank = 0;
     let indexCandidateStart = 0;
+    let stationsTraveled = 0;
     while(indexCandidateStart < gas.length){
-        if(cost[indexCandidateStart] > gas[indexCandidateStart]){
+        if (gas[indexCandidateStart] < cost[indexCandidateStart]) {
             indexCandidateStart++;
         } else {
-            tank = gas[indexCandidateStart];
-            end = (indexCandidateStart+1) % gas.length;
-            let prevEnd = end === 0 ? gas.length-1 : end-1;
-            
-            while(tank - cost[prevEnd] + gas[end] >= cost[end] && indexCandidateStart !== end){
+            start = indexCandidateStart;
+            tank = gas[start];
+            end = start % gas.length;
+            while (tank >= cost[end] && stationsTraveled < gas.length) {
+                end = (end + 1) % gas.length;
+                prevEnd = (end === 0) ? gas.length - 1 : end - 1;
                 tank = tank - cost[prevEnd] + gas[end];
-                end = (end+1) % gas.length;
-                prevEnd = end === 0 ? gas.length-1 : end-1;
-            }
+                stationsTraveled++;
 
-            if(indexCandidateStart === end){
-                start = indexCandidateStart;
-                indexCandidateStart = gas.length;
-            } else {
-                indexCandidateStart += 1;
+                console.log('prevEnd:', prevEnd);
             }
+            
+            if (tank < cost[end]){
+                // console.log('tank:', tank);
+                // console.log('cost[end]:', cost[end]);
+                // console.log('start:', start);
+                indexCandidateStart = start + 1;
+                start = -1;
+                stationsTraveled = 0;
+                // console.log('indexCandidateStart', indexCandidateStart)
+            } else {
+                console.log('tank:', tank);
+                indexCandidateStart = gas.length;
+            } 
         }
     }
     return start;
 }
 
-let result = canCompleteCircuit([2,3,4], [3,4,3]);
-console.log('res:', result)
+// let completeCircuit = canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]);
+// console.log('res:', completeCircuit);
 
-//            s
-//        e                e
-// gas = [2,3,4], costo = [3,4,3]
+// let completeCircuit = canCompleteCircuit([2,3,4], [3,4,3]);
+// console.log('res:', completeCircuit);
 
-// tank = 4 star = 2 end = 0 prevEnd = 2
-// tank = 4 - 3 + 2 = 3 >= 3 si -> tank = 3 end = 1 -> 0   
+let completeCircuit = canCompleteCircuit([4,5,2,6,5,3], [3,2,7,3,2,9]);
+console.log('res:', completeCircuit);
 
 
-// 0 -> nums.length - 1 -> 2
-// 1 -> nums.length - 1 -> 0
-// 2 -> nums.length - 1 -> 1
 
-// mientras el costo sea mayor moveria el inicio hasta encontrar 
-// un indice donde comenzar
 
-// tanque = 4(tanque) - 3(costo[end-1]) + 2(gas[end]) = 3 va de 0 a 1 porque costo[end] = 3 es <= tanque = 3 y avanzo e  
-// tanque = 3(tanque) - 3(costo[end-1]) + 3(gas[end]) = 3 no va de 1 a 2 porque costo[1] = 4 es >= tanque = 3 
+// st = 3
+// t = 6
+//              s                    s
+//            e                    e
+// gas = [4,5,2,6,5,3] cost = [3,2,7,3,2,9]
+
+
+// if(cost[indexCandidateStart] > gas[indexCandidateStart]){
+//     indexCandidateStart++;
+// } else {
+//     tank = gas[indexCandidateStart];
+//     end = (indexCandidateStart+1) % gas.length;
+//     let prevEnd = end === 0 ? gas.length-1 : end-1;
+    
+//     while(tank - cost[prevEnd] + gas[end] >= cost[end] && indexCandidateStart !== end){
+//         tank = tank - cost[prevEnd] + gas[end];
+//         end = (end+1) % gas.length;
+//         prevEnd = end === 0 ? gas.length-1 : end-1;
+//     }
+
+//     if(indexCandidateStart === end){
+//         start = indexCandidateStart;
+//         indexCandidateStart = gas.length;
+//     } else {
+//         indexCandidateStart += 1;
+//     }
+// }
